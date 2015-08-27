75c8b77a6b071b5f071810492ddc919172e55047
Ryan Curtin <ryan@ratml.org>
2015-08-27 19:40:30 -0400

    Better labeling of doxygen template output.

--- docs/doxygen-post/test_template_annotator.py
+++ docs/doxygen-post/test_template_annotator.py
@@ -6,10 +6,10 @@ a = t.grammar.parseString('template &lt; typename A &gt;')
 print a.asXML('div')
 print ''
 
-a = t.grammar.parseString('template &lt; &gt;')
+#a = t.grammar.parseString('template &lt; &gt;')
 
-print a.asXML('div')
-print ''
+#print a.asXML('div')
+#print ''
 
 a = t.grammar.parseString('template &lt; typename A, typename B &gt;')
 
@@ -32,6 +32,12 @@ print a.asXML('div')
 print ''
 
 a = t.grammar.parseString('template &lt; template &lt; typename A &gt; class B &gt;')
+
+print a.asXML('div')
+print ''
+
+a = t.grammar.parseString('template&lt;typename MetricType, typename StatisticType = EmptyStatistic, typename MatType = arma::mat, template&lt; typename BoundMetricType &gt; class BoundType = bound::HRectBound, template&lt; typename SplitBoundType, typename SplitMatType &gt; class SplitType = MidpointSplit &gt;')
+#a = t.grammar.parseString('template&lt;template&lt; typename BoundMetricType &gt; class BoundType = bound::HRectBound, template&lt; typename SplitBoundType, typename SplitMatType &gt; class SplitType = MidpointSplit &gt;')
 
 print a.asXML('div')
 print ''

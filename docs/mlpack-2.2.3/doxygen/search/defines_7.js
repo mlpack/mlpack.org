f7c154beaa53ecd2e6e8e3e255a9761ec11f4bc4
Ryan Curtin <ryan@ratml.org>
2017-05-24 11:24:19 -0400

    Release 2.2.3.

---
+++ docs/mlpack-2.2.3/doxygen/search/defines_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['typename',['TYPENAME',['../cli_8hpp.html#ac909d914008cce49fa42ddd10b9b36c5',1,'cli.hpp']]]
+];
---
+++ docs/mlpack-2.2.3/doxygen/search/typedefs_13.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vec',['Vec',['../classmlpack_1_1bound_1_1BallBound.html#a80bd22c7f2db7dd90f256148ecd6eb5f',1,'mlpack::bound::BallBound']]],
+  ['vptree',['VPTree',['../namespacemlpack_1_1tree.html#a8aa23faa8b1343b19a123535cfc85b9a',1,'mlpack::tree']]],
+  ['vptreesplit',['VPTreeSplit',['../namespacemlpack_1_1tree.html#a9c6891d010c7e41d1de1bbb25466b735',1,'mlpack::tree']]]
+];

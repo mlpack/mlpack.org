b388ce7566cb44147ffb992788c6e377c5b75642
Ryan Curtin <ryan@ratml.org>
2019-11-12 13:28:45 -0500

    Be safer about processing names with [] characters.

--- irc/scripts/process-log.sh
+++ irc/scripts/process-log.sh
@@ -24,6 +24,8 @@ nameslist=`cat $logfile | grep -E '^[0-9]{2}:[0-9]{2} < ' |
 # shit?".  Haha!  Sucks!  Have fun!
 cat $logfile | sed -E 's/</\&lt;/g' |
                sed -E 's/>/\&gt;/g' |
+               sed -E 's/\[/\&#91;/g' |
+               sed -E 's/\]/\&#93:/g' |
                sed -E 's/$/<\/font><br>/' |
                sed -E 's/-!-/<\/font><font color="#aaaaaa">-!-<\/font><font color="#666666">/' |
                sed -E 's/^/<font color="#bb2222">/' > /tmp/build-page-tmp-0;
@@ -32,7 +34,9 @@ cat $logfile | sed -E 's/</\&lt;/g' |
 counter=0
 for i in $nameslist;
 do
-  cat /tmp/build-page-tmp-$counter | sed -E 's/(&lt; '$i'&gt;)/<\/font><font class="ircnick'$counter'">\1<\/font><font color="#aaaaaa">/' > /tmp/build-page-tmp-$(($counter + 1));
+  # Filter out [ and ] from names...
+  safename=`echo $i | sed 's/\[/\&#91;/g' | sed 's/\]/\&#93;/g'`
+  cat /tmp/build-page-tmp-$counter | sed -E "s/(&lt; "$safename"&gt;)/<\/font><font class=\"ircnick"$counter"\">\1<\/font><font color=\"#aaaaaa\">/" > /tmp/build-page-tmp-$(($counter + 1));
   counter=$(($counter + 1));
 done
 

b200de7805ea90310a24197cb74d5f71a0db2558
Ryan Curtin <ryan@ratml.org>
2019-12-25 11:42:33 -0500

    Fix IRC CSS.

--- irc/scripts/process-log.sh
+++ irc/scripts/process-log.sh
@@ -25,9 +25,9 @@ nameslist=`cat $logfile | grep -E '^[0-9]{2}:[0-9]{2} < ' |
 cat $logfile | sed -E 's/</\&lt;/g' |
                sed -E 's/>/\&gt;/g' |
                sed -E 's/\[/\&#91;/g' |
-               sed -E 's/\]/\&#93:/g' |
+               sed -E 's/\]/\&#93;/g' |
                sed -E 's/$/<\/font><br>/' |
-               sed -E 's/-!-/<\/font><font color="#aaaaaa">-!-<\/font><font color="#666666">/' |
+               sed -E 's/-!-/<\/font><font color="#aaa">-!-<\/font><font color="#aaa">/' |
                sed -E 's/^/<font color="#bb2222">/' > /tmp/build-page-tmp-0;
 
 # Now set a color for each particular nick that was seen that day.
@@ -35,8 +35,8 @@ counter=0
 for i in $nameslist;
 do
   # Filter out [ and ] from names...
-  safename=`echo $i | sed 's/\[/\&#91;/g' | sed 's/\]/\&#93;/g'`
-  cat /tmp/build-page-tmp-$counter | sed -E "s/(&lt; "$safename"&gt;)/<\/font><font class=\"ircnick"$counter"\">\1<\/font><font color=\"#aaaaaa\">/" > /tmp/build-page-tmp-$(($counter + 1));
+  safename=`echo $i | sed 's/\[/\&#91;/g' | sed 's/\]/\&#93;/g'` 
+  cat /tmp/build-page-tmp-$counter | sed -E "s/(&lt; "$safename"&gt;)/<\/font><font class=\"ircnick"$counter"\">\1<\/font><font color=\"#616161\">/" > /tmp/build-page-tmp-$(($counter + 1));
   counter=$(($counter + 1));
 done
 

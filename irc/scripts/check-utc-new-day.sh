23c75c88931e5f19576d1913419e36cd75bea6a9
Ryan Curtin <ryan@ratml.org>
2020-09-01 17:57:59 -0400

    Fix links and update script to run at different time.

--- irc/scripts/check-utc-new-day.sh
+++ irc/scripts/check-utc-new-day.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Check if it's between 12am and 1am UTC.  If so, run the script given as an
+# Check if it's between 5am and 6am UTC.  If so, run the script given as an
 # argument.
 curhour=`date --utc -I'seconds' | sed -E 's/.*T([0-9]{2}):[0-9]{2}:[0-9]{2}.*/\1/'`;
 
-if [ "a$curhour" = "a00" ]; then
+if [ "a$curhour" = "a05" ]; then
   $@;
 fi
